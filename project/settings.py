3bdaf5e016c5948e1bd675531539092a516a591f
jendox <jendox1985@gmail.com>
2023-04-15 13:59:14 +0300

    added project setting.py

---
+++ project/settings.py
@@ -0,0 +1,43 @@
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv("DATABASE_ENGINE"),
+        'HOST': os.getenv("DATABASE_HOST"),
+        'PORT': os.getenv("DATABASE_PORT"),
+        'NAME': os.getenv("DATABASE_NAME"),
+        'USER': os.getenv("DATABASE_USER"),
+        'PASSWORD': os.getenv("DATABASE_PASSWORD"),
+    }
+}
+
+INSTALLED_APPS = ['datacenter']
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DEBUG = True
+
+ROOT_URLCONF = 'project.urls'
+
+ALLOWED_HOSTS = ['*']
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+    },
+]
+
+
+USE_L10N = True
+
+LANGUAGE_CODE = 'ru-ru'
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_TZ = True
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
